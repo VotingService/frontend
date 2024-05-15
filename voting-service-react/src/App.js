ce9120731b70e0efa2447b249c19af078b3d3307
Ustym Buchko <92269527+Ustymko@users.noreply.github.com>
2024-05-15 13:44:13 +0300

    cleaned App file

--- voting-service-react/src/App.js
+++ voting-service-react/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {
     BrowserRouter as Router,
@@ -16,22 +15,6 @@ function App() {
                 <Route path="/register" element={<Register/>}/>
             </Routes>
         </Router>
-        // <div className="App">
-        //   <header className="App-header">
-        //     <img src={logo} className="App-logo" alt="logo" />
-        //     <p>
-        //       Edit <code>src/App.js</code> and save to reload.
-        //     </p>
-        //     <a
-        //       className="App-link"
-        //       href="https://reactjs.org"
-        //       target="_blank"
-        //       rel="noopener noreferrer"
-        //     >
-        //       Learn React
-        //     </a>
-        //   </header>
-        // </div>
     );
 }
 
