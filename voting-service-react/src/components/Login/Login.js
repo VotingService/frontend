c60c6df0d294827dc8aedfad64904e402bd43fad
Ustym Buchko <92269527+Ustymko@users.noreply.github.com>
2024-05-15 18:31:40 +0300

    Login screen layout

--- voting-service-react/src/components/Login/Login.js
+++ voting-service-react/src/components/Login/Login.js
@@ -1,6 +1,33 @@
-function Login(){
-    return(
-        <div>Login page</div>
+import { HiOutlineMailOpen } from "react-icons/hi";
+import { FaLock } from "react-icons/fa";
+import "./Login.css"
+
+function Login() {
+    return (
+        <div className='wrapper'>
+            <form action=''>
+                <h1>Авторизація</h1>
+                <div className='input-box'>
+                    <input type="email" placeholder='Електронна адреса' required/>
+                    <HiOutlineMailOpen className="icon"/>
+                </div>
+                <div className='input-box'>
+                    <input type="password" placeholder='Пароль' required/>
+                    <FaLock className="icon"/>
+                </div>
+
+                <div className="remember-forgot">
+                    <label><input type="checkbox"/>Запам'ятати мене</label>
+                    <a href="#">Забули пароль?</a>
+                </div>
+
+                <button type="submit">Увійти</button>
+
+                <div className="register-link">
+                    <p>Вперше тут? <a href="#">Зареєструйтесь</a></p>
+                </div>
+            </form>
+        </div>
     )
 }
 
