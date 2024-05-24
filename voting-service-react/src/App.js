07fc70a78ff5119888c1858451c8e38fe50d5887
Ustym Buchko <92269527+Ustymko@users.noreply.github.com>
2024-05-24 23:19:26 +0300

    header layout

--- voting-service-react/src/App.js
+++ voting-service-react/src/App.js
@@ -6,10 +6,12 @@ import {
 } from "react-router-dom"
 import Login from './components/Authorization/Login/Login'
 import Register from './components/Authorization/Register/Register'
+import Header from './components/Header/Header';
 
 function App() {
     return (
         <Router>
+            <Header/>
             <Routes>
                 <Route path="/" element={<Login/>}/>
                 <Route path="/register" element={<Register/>}/>
---
+++ voting-service-react/src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.css';
+import { MdAccountCircle } from "react-icons/md";
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              <MdAccountCircle style={{width: 22, height: 22}}/>
+              Особистий кабінет
+            </a>
+          </li>
+          <li className="nav-item"><a href="#" className="nav-link">Доступні вибори</a></li>
+          <li className="nav-item"><a href="#" className="nav-link">Історія голосів</a></li>
+          <li className="nav-item"><a href="#" className="nav-link">Про платформу</a></li>
+          <li className="nav-item exit-button"><a href="#" className="nav-link">Вийти</a></li>
+        </ul>
+        
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
