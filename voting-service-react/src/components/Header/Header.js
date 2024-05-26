c4808f6463e9bf786d5aae8a38486f579a2d8007
Ustym Buchko <92269527+Ustymko@users.noreply.github.com>
2024-05-26 11:47:13 +0300

    routing from header to account page

--- voting-service-react/src/components/Header/Header.js
+++ voting-service-react/src/components/Header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
               Особистий кабінет
             </a>
           </li>
-          <li className="nav-item"><a href="#" className="nav-link">Доступні вибори</a></li>
+          <li className="nav-item"><a href="/account" className="nav-link">Доступні вибори</a></li>
           <li className="nav-item"><a href="#" className="nav-link">Історія голосів</a></li>
           <li className="nav-item"><a href="/about" className="nav-link">Про платформу</a></li>
           <li className="nav-item exit-button"><a href="#" className="nav-link">Вийти</a></li>
