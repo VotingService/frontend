8553c5dfd0272d27cdfbca0034b5980104b83d79
Ustym Buchko <92269527+Ustymko@users.noreply.github.com>
2024-05-26 11:48:22 +0300

    fixed route from header to account page

--- voting-service-react/src/components/Header/Header.js
+++ voting-service-react/src/components/Header/Header.js
@@ -8,12 +8,12 @@ const Header = () => {
       <nav className="navbar">
         <ul className="nav-list">
           <li className="nav-item">
-            <a href="#" className="nav-link">
+            <a href="/account" className="nav-link">
               <MdAccountCircle style={{width: 22, height: 22}}/>
               Особистий кабінет
             </a>
           </li>
-          <li className="nav-item"><a href="/account" className="nav-link">Доступні вибори</a></li>
+          <li className="nav-item"><a href="#" className="nav-link">Доступні вибори</a></li>
           <li className="nav-item"><a href="#" className="nav-link">Історія голосів</a></li>
           <li className="nav-item"><a href="/about" className="nav-link">Про платформу</a></li>
           <li className="nav-item exit-button"><a href="#" className="nav-link">Вийти</a></li>
