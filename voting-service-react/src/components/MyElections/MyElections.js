35165e3c46111db033d3071df215a402af6e9044
BogdanZavadka <bogdanzavadka@gmail.com>
2024-05-27 03:07:07 +0300

    search and sort to my election page

--- voting-service-react/src/components/MyElections/MyElections.js
+++ voting-service-react/src/components/MyElections/MyElections.js
@@ -1,12 +1,15 @@
 import "./MyElections.css"
 import AdminPanel from "../AdminPanel/AdminPanel";
 import Blocks from "../Home/Blocks/Blocks";
+import {useState} from "react";
 
 export default function MyElections(){
+    const [sortBy, setSortBy] = useState({sortBy: ''});
+    const [searchValue, setSearchValue] = useState({searchValue: ''});
     let myElections = [
         {name: 'Вибори президента України', buttonType: 'history', endDate: '01/01/2022'},
-        {name: 'Історія голосування2', buttonType: 'history', endDate: '02/02/2023'},
-        {name: 'Історія голосування2', buttonType: 'history', endDate: '02/02/2023'},
+        {name: 'фсторія голосування2', buttonType: 'history', endDate: '02/02/2023'},
+        {name: 'асторія голосування2', buttonType: 'history', endDate: '02/02/2023'},
         {name: 'Історія голосування2', buttonType: 'history', endDate: '02/02/2023'},
         {name: 'Історія голосування2', buttonType: 'history', endDate: '02/02/2023'},
         {name: 'Історія голосування2', buttonType: 'history', endDate: '03/03/2024'}
@@ -19,17 +22,19 @@ export default function MyElections(){
                 <div className="sort">
                     <div className="sort-by">Сортувати за:</div>
                     <div className="sort-buttons">
-                        <button>Назвою</button>
-                        <button>Датою початку</button>
-                        <button>Датою кінця</button>
-                        <button>Локацією</button>
+                        <button onClick={() => setSortBy({ sortBy: 'name'})}>Назвою</button>
+                        <button onClick={() => setSortBy({ sortBy: 'startDate'})}>Датою початку</button>
+                        <button onClick={() => setSortBy({ sortBy: 'endDate'})}>Датою кінця</button>
+                        <button onClick={() => setSortBy({ sortBy: 'location'})}>Локацією</button>
                     </div>
                 </div>
                 <div>
-                    <input type="text" placeholder="Пошук.."/>
+                    <input type="text" placeholder="Пошук.." onChange=
+                        {(value) => setSearchValue(
+                            {searchValue: value.target.value})}/>
                     <button className="search-button">Пошук</button>
                 </div>
-                <Blocks blocks={myElections}></Blocks>
+                <Blocks blocks={myElections} sortBy={sortBy} searchValue={searchValue}></Blocks>
             </div>
         </div>
     )
