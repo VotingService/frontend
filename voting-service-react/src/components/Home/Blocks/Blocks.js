11f5975f6f1fafe64d62180917abf0b0b4e98c4e
BogdanZavadka <bogdanzavadka@gmail.com>
2024-05-27 03:06:37 +0300

    added search and sort to my election page

--- voting-service-react/src/components/Home/Blocks/Blocks.js
+++ voting-service-react/src/components/Home/Blocks/Blocks.js
@@ -2,10 +2,42 @@ import "./blocks.css"
 import Block from "./Block";
 
 export default function Blocks(props){
-    let blocks = props.blocks;
-    let renderedBlocks = blocks.map(item =>
+    let sortBy = props.sortBy.sortBy;
+    let searchValue = props.searchValue.searchValue;
+    let itemList = props.blocks;
+    let foundItems = itemList.filter(item => item.name.search(searchValue) !== -1);
+    let sortedItems = [];
+    if (sortBy === 'name'){
+        sortedItems = foundItems.sort(function (a, b) {
+            return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
+        })}
+    else{
+        sortedItems = foundItems;
+    }
+    if (sortBy === 'startDate'){
+        sortedItems = foundItems.sort(function (a, b) {
+            return a.startDate > b.startDate ? 1 : -1;
+        })}
+    else{
+        sortedItems = foundItems;
+    }
+    if (sortBy === 'endDate'){
+        sortedItems = foundItems.sort(function (a, b) {
+            return a.endDate > b.endDate ? 1 : -1;
+        })}
+    else{
+        sortedItems = foundItems;
+    }
+    if (sortBy === 'location'){
+        sortedItems = foundItems.sort(function (a, b) {
+            return a.location.toLowerCase() > b.location.toLowerCase() ? 1 : -1;
+        })}
+    else{
+        sortedItems = foundItems;
+    }
+    let renderedBlocks = sortedItems.map(item =>
         <Block name={item.name} buttonType={item.buttonType} endDate={item.endDate}
-               key={blocks.indexOf(item)}></Block>)
+               key={sortedItems.indexOf(item)}></Block>)
     return(
         <div className="available-blocks">
             {renderedBlocks}
