5dbb08bbb27fc5833b56255ad6cca85373e8a552
Ustym Buchko <92269527+Ustymko@users.noreply.github.com>
2024-05-27 21:51:00 +0300

    single vote election type

--- voting-service-react/src/components/CandidateList/CandidateList.js
+++ voting-service-react/src/components/CandidateList/CandidateList.js
@@ -1,29 +1,51 @@
 import "./CandidateList.css"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function CandidateList(){
-    let candidates = [{name: 'Зеленський Володимир Олександрович'},
-        {name: 'Зеленський Володимир Олександрович'},
-        {name: 'Зеленський Олександрович'},
-        {name: 'Зеленський Володимир Олександрович'},
-        {name: 'Зеленський Володимир Олександрович'},
-        {name: 'Зеленський Володимир Олександрович'}
+    let candidates = [{id: 1, name: 'Зеленський Володимир Олександрович'},
+        {id: 2, name: 'Зеленський Володимир Олександрович'},
+        {id: 3, name: 'Зеленський Олександрович'},
+        {id: 4, name: 'Зеленський Володимир Олександрович'},
+        {id: 5, name: 'Зеленський Володимир Олександрович'},
+        {id: 6, name: 'Зеленський Володимир Олександрович'}
     ];
-    let renderedCandidates = candidates.map(candidate =>
-        <div>
-            <li>
-                <div className="candidate-name">{candidate.name}</div>
-                <a>Про кандидата</a>
-                <button>Проголосувати</button>
-            </li>
-            <hr/>
-        </div>)
+    const [selectedCandidate, setSelectedCandidate] = useState(null);
+
+    const handleVoteChange = (e) => {
+        setSelectedCandidate(e.target.value);
+    };
+
+    const handleSubmit = () => {
+        console.log('Selected Candidate ID:', selectedCandidate);
+        // Add logic to submit the selected candidate vote to the server
+    };
     return (
-        <div className="CandidateList">
-            <h1>Список кандидатів для голосування</h1>
-            <ul>
-                {renderedCandidates}
-            </ul>
-            <div className="bottom"></div>
+        <div className="vote-for-candidate-page">
+            <h1 className="header">Голосування</h1>
+            <h4>Щоб віддати голос, натисніть на кружечок навпроти імені вибраного кандидата</h4>
+            <div className="candidate-list">
+        {candidates.map((candidate) => (
+            <div key={candidate.id} className="candidate-item">
+                <h3>{candidate.name}</h3>
+                <div className="candidate-item__about-and-input">
+                    <Link className="about" to={"/candidate"}>Про кандидата</Link>
+                    <input
+                    type="radio"
+                    name="candidate"
+                    value={candidate.id}
+                    checked={selectedCandidate === candidate.id.toString()}
+                    onChange={handleVoteChange}
+                    />
+                    <span className="custom-radio"></span>
+                </div>
+            </div>
+        ))}
         </div>
-    )
+        <button className="submit-button" onClick={handleSubmit} disabled={!selectedCandidate}>
+            Проголосувати
+        </button>
+    </div>
+    );
+    
 }
