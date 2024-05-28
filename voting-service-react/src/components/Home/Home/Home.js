f0e6712157954a0642c8a2121074f58bfae9d122
BogdanZavadka <bogdanzavadka@gmail.com>
2024-05-28 22:55:43 +0300

    completed home page soon elections from backend

--- voting-service-react/src/components/Home/Home/Home.js
+++ voting-service-react/src/components/Home/Home/Home.js
@@ -6,20 +6,29 @@ import {useEffect, useState} from "react";
 import Block from "../Blocks/Block";
 
 export default function Home(props) {
-    const [renderedBlocks, setRenderedBlocks] = useState([]);
+    const [renderedAvailableBlocks, setRenderedAvailableBlocks] = useState([]);
+    const [renderedSoonBlocks, setRenderedSoonBlocks] = useState([]);
     useEffect(() => {
         getUserCanParticipateInElections(sessionStorage.getItem("user_id"),
             {Authorization: `Bearer ${sessionStorage.getItem("auth_token")}`}).then(res => {
             available = res.data._embedded.elections.map((item) => {
-                return ({name: item.title, buttonType: 'vote'})
+                return ({name: item.title, buttonType: 'vote', startDate: item.startDate})
             });
-            setRenderedBlocks(available);
+            setRenderedAvailableBlocks(available);
+            let soon1 = [];
+            const firstDate = '2024-05-28T22:43:46';
+            soon1 = available.filter(item => item.startDate > '2024-05-28T22:43:46');
+            setRenderedSoonBlocks(soon1);
         }).catch((error) => {
         });
+
     }, []);
-    let renderedBlocks1 = renderedBlocks.map(item =>
+    let renderedAvailableBlocks1 = renderedAvailableBlocks.map(item =>
         <Block name={item.name} buttonType={item.buttonType} endDate={item.endDate}
-               key={renderedBlocks.indexOf(item)}></Block>);
+               key={renderedAvailableBlocks.indexOf(item)}></Block>);
+    let renderedSoonBlocks1 = renderedSoonBlocks.map(item =>
+        <Block name={item.name} buttonType={item.buttonType} endDate={item.endDate}
+               key={renderedSoonBlocks.indexOf(item)}></Block>);
     let available = [
         // {name: 'Вибори президента України', buttonType: 'vote'},
         // {name: 'Доступне голосування2', buttonType: 'vote'},
@@ -32,24 +41,18 @@ export default function Home(props) {
         // {name: 'Скоро голосування1', buttonType: 'soon'},
         // {name: 'Скоро голосування2', buttonType: 'soon'}
     ];
-    let currently = [
-        // {name: 'Зараз голосування1', buttonType: 'currently'},
-        // {name: 'Зараз голосування2', buttonType: 'currently'}
-    ];
     return (
         <div>
             <Header/>
             <div className="Home">
                 <h1 className="available">Доступні для вас голосування:</h1>
-                {renderedBlocks1.length === 0 ?
+                {renderedAvailableBlocks1.length === 0 ?
                     <div className="no-available">Зараз немає доступних для вас голосувань.</div> :
-                    <div className="available-blocks">{renderedBlocks1}</div>}
+                    <div className="available-blocks">{renderedAvailableBlocks1}</div>}
                 <h1 className="soon">Скоро відбудуться:</h1>
-                {soon.length === 0 ? <div className="no-available">Зараз немає доступних голосувань.</div> :
-                    <Blocks blocks={soon} sortBy={''} searchValue={''} className="available-blocks"></Blocks>}
-                <h1 className="soon">Відбуваються зараз:</h1>
-                {currently.length === 0 ? <div className="no-available">Зараз немає доступних голосувань.</div> :
-                    <Blocks blocks={currently} sortBy={''} searchValue={''} className="available-blocks"></Blocks>}
+                {renderedSoonBlocks1.length === 0 ?
+                    <div className="no-available">Зараз немає доступних голосувань.</div> :
+                    <div className="available-blocks">{renderedSoonBlocks1}</div>}
                 <div className="bottom"></div>
             </div>
         </div>
