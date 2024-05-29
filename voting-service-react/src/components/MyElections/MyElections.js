907abe8707a6f4f97fc05af32eedcb0774fe8072
Ustym Buchko <92269527+Ustymko@users.noreply.github.com>
2024-05-29 04:55:03 +0300

    removed dummy data array and sorting by location

--- voting-service-react/src/components/MyElections/MyElections.js
+++ voting-service-react/src/components/MyElections/MyElections.js
@@ -10,11 +10,6 @@ export default function MyElections(){
     const [sortBy, setSortBy] = useState({sortBy: ''});
     const [searchValue, setSearchValue] = useState({searchValue: ''});
     const [elections, setElections] = useState([]);
-    let myElections = [
-        {name: 'Вибори президента України', buttonType: 'history', endDate: '01/01/2022'},
-        {name: 'Історія голосування2', buttonType: 'history', endDate: '02/02/2023'},
-        {name: 'Історія голосування2', buttonType: 'history', endDate: '02/02/2023'},
-    ];
     useEffect(() => {
         if(sessionStorage.getItem("role") === 'USER'){
             navigate("/home")
@@ -31,18 +26,8 @@ export default function MyElections(){
                 startDate: obj.startDate.split('T')[0],
                 endDate: obj.endDate.split('T')[0],
                 name: obj.title,
-                buttonType: 'history' // Add any new key-value pair you need here
+                buttonType: 'history'
             })))
-            // rename title to name
-            // setElections(elections.map(obj => {
-            //     const {title, ...rest} = obj;
-            //     return {...rest, name: title};
-            // }))
-            // add buttonType parameter to each object
-            // setElections(elections.map(obj => {
-            //     return {...obj, buttonType: 'history'}
-            // }))
-            console.log(res.data._embedded.elections)
         })
         .catch((err) => {
             console.log(err)
@@ -59,7 +44,6 @@ export default function MyElections(){
                         <button onClick={() => setSortBy({ sortBy: 'name'})}>Назвою</button>
                         <button onClick={() => setSortBy({ sortBy: 'startDate'})}>Датою початку</button>
                         <button onClick={() => setSortBy({ sortBy: 'endDate'})}>Датою кінця</button>
-                        <button onClick={() => setSortBy({ sortBy: 'location'})}>Локацією</button>
                     </div>
                 </div>
                 <div>
