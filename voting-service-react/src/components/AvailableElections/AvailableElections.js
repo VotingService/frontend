afcaa6c7bddb6f79b53d6b2912802c824279da82
BogdanZavadka <bogdanzavadka@gmail.com>
2024-05-29 09:51:21 +0300

    connected available election page with backend

--- voting-service-react/src/components/AvailableElections/AvailableElections.js
+++ voting-service-react/src/components/AvailableElections/AvailableElections.js
@@ -2,7 +2,9 @@ import "./AvailableElections.css"
 import Blocks from "../Home/Blocks/Blocks";
 import Header from "../Header/Header";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
+import {getUserCanParticipateInElections} from "../../API/API";
+import Block from "../Home/Blocks/Block";
 
 export default function AvailableElections() {
   const navigate = useNavigate()
@@ -21,13 +23,29 @@ export default function AvailableElections() {
       navigate("/")
   }
   })
+  const [renderedAvailableBlocks, setRenderedAvailableBlocks] = useState([]);
+  useEffect(() => {
+    getUserCanParticipateInElections(sessionStorage.getItem("user_id"),
+        {Authorization: `Bearer ${sessionStorage.getItem("auth_token")}`}).then(res => {
+      let av1 = res.data._embedded.elections.filter(item => item.startDate < '2024-05-28T22:43:46');
+      available = av1.map((item) => {
+        return ({name: item.title, buttonType: 'vote', startDate: item.startDate, electionId: item.id})
+      });
+      setRenderedAvailableBlocks(available);
+    }).catch((error) => {
+    });
+
+  }, []);
+  let renderedAvailableBlocks1 = renderedAvailableBlocks.map(item =>
+      <Block name={item.name} buttonType={item.buttonType} endDate={item.endDate} electionId={item.electionId}
+             key={renderedAvailableBlocks.indexOf(item)}></Block>);
   return(
       <div>
         <Header/>
         <div className="Home">
           <h1 className="available">Доступні для вас голосування:</h1>
           {available.length === 0 ? <div className="no-available">Зараз немає доступних для вас голосувань.</div> :
-              <Blocks blocks={available} sortBy={"endDate"} className="available-blocks"></Blocks>}
+              <div className="available-blocks">{renderedAvailableBlocks1}</div>}
         </div>
       </div>
   )
